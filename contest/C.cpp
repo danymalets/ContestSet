98c5765800b58e3a9929fab29daf4247a512a29d
danymalets <malets.d.l@gmail.com>
2021-11-14 09:42:12 +0300

    geom

--- contest/C.cpp
+++ contest/C.cpp
@@ -9,7 +9,8 @@ typedef long double ld;
 
 #define fast_io ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 #define erun(x) (x).erase(unique(all(x)),(x).end())
-#define sp(x) cout << setprecision(x) << fixed;
+#define sp(x) setprecision(x) << fixed
+#define spp(x) cout << setprecision(x) << fixed
 #define all(x) (x).begin(),(x).end()
 #define dcl(x,y) ((x)%(y)?(x)/(y)+1:(x)/(y))
 #define cube(x) ((x)*(x)*(x))
@@ -24,19 +25,45 @@ typedef long double ld;
 #define file_all file_in,file_out
 #define file_in freopen("input.txt","rt",stdin)
 #define file_out freopen("output.txt","wt",stdout)
-#define print_time() sp(2); cerr << "\nTime execute: " << clock() / (double)CLOCKS_PER_SEC << " sec\n";
+#define print_time() cerr << sp(2) << "\nTime execute: " << clock() / (double)CLOCKS_PER_SEC << " sec\n";
+
+int gcd(int x, int y){ return __gcd(x, y); }
+int gcd(ll x, ll y){ return __gcd(x, y); }
+ll lcm(int x, int y){ return 1LL * x * y / gcd(x, y); }
+
+ld root(ld n, ld x){ return pow(x, 1 / n); }
+ld log(ld n, ld x){ return log(x) / log(n); }
+int pow2(int n){ return (1 << n); }
+ll pow2ll(int n){ return (1LL << n); }
+bool is_pow2(int x){ return !(x&(x-1)); }
+bool is_pow2(ll x){ return !(x&(x-1)); }
+bool is_sqr(int x){ int t = sqrt(x); return t * t == x; }
+bool is_sqr(ll x){ int t = sqrt(x); return 1LL * t * t == x; }
+
+int bin_pow(int x, int n, int mod){
+    if (n == 0) return 1 % mod;
+    if (n % 2 == 1) return 1LL * bin_pow(x, n - 1, mod) * x % mod;
+    int t = bin_pow(x, n / 2, mod);
+    return t * t % mod;
+}
+
+int bin_pow(int x, ll n, int mod){
+    if (n == 0) return 1 % mod;
+    if (n % 2 == 1) return 1LL * bin_pow(x, n - 1, mod) * x % mod;
+    int t = bin_pow(x, n / 2, mod);
+    return t * t % mod;
+}
 
 void print() { }
-template<typename First, typename... Strings>
-void print(First arg, const Strings&... rest) {
-    cout << arg << " "; print(rest...);
-}
-
+template<typename First, typename... Strings> void print(First arg, const Strings&... rest) { cout << arg << " "; print(rest...); }
 void println() { cout << '\n'; }
-template<typename First, typename... Strings>
-void println(First arg, const Strings&... rest) {
-    cout << arg << " "; println(rest...);
-}
+template<typename First, typename... Strings> void println(First arg, const Strings&... rest) { cout << arg << " "; println(rest...); }
+template<typename T> void printv(vector<T> v) { for (int i = 0; i < v.size(); i++) print(v[i]); }
+template<typename T> void printvln(vector<T> v) { printv(v); println(); }
+template<typename T> void printa(T a[], int l, int r) { for (int i = l; i < r; i++) print(a[i]); }
+template<typename T> void printaln(T a[], int l, int r) { printa(a, l, r); println(); }
+template<typename T> void printa(T a[], int n) { printa(a, 0, n); }
+template<typename T> void printaln(T a[], int n) { printaln(a, 0, n); }
 
 #pragma endregion TEMPLATE
 
@@ -47,11 +74,9 @@ const int MOD = 1e9+7;
 const int M = 1000;
 const int N = 2e5;
 
-int
 
 int main() {
     fast_io;
-
 
 
     return 0;
